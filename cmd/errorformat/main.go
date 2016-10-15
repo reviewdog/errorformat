4d0bf7e796e9d7b6e8be9dd102347a90a2b430d6
haya14busa <hayabusa1419@gmail.com>
2016-10-16 04:29:18 +0900

    cmd: add format feature

--- cmd/errorformat/main.go
+++ cmd/errorformat/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/haya14busa/errorformat"
 )
@@ -19,6 +22,36 @@ Example:
 	$ echo '/path/to/file:14:28: error message\nfile2:3:4: msg' | errorformat "%f:%l:%c: %m"
 	/path/to/file|14 col 28| error message
 	file2|3 col 4| msg
+
+The -f flag specifies an alternate format for the entry, using the
+syntax of package template.  The default output is equivalent to -f
+'{{.String}}'. The struct being passed to the template is:
+
+	type Entry struct {
+		// name of a file
+		Filename string
+		// line number
+		Lnum int
+		// column number (first column is 1)
+		Col int
+		// true: "col" is visual column
+		// false: "col" is byte index
+		Vcol bool
+		// error number
+		Nr int
+		// search pattern used to locate the error
+		Pattern string
+		// description of the error
+		Text string
+		// type of the error, 'E', '1', etc.
+		Type rune
+		// true: recognized error message
+		Valid bool
+
+		// Original error lines (often one line. more than one line for multi-line
+		// errorformat. :h errorformat-multi-line)
+		Lines []string
+	}
 `
 
 func usage() {
@@ -28,10 +61,24 @@ func usage() {
 	os.Exit(2)
 }
 
+var entryFmt = flag.String("f", "{{.String}}", "format template")
+
 func main() {
 	flag.Usage = usage
 	flag.Parse()
 	errorformats := flag.Args()
+
+	out := newTrackingWriter(os.Stdout)
+
+	fm := template.FuncMap{
+		"join": strings.Join,
+	}
+	tmpl, err := template.New("main").Funcs(fm).Parse(*entryFmt)
+	if err != nil {
+		fatalf("%s\n", err)
+	}
+	_ = tmpl
+
 	efm, err := errorformat.NewErrorformat(errorformats)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -39,6 +86,49 @@ func main() {
 	}
 	s := efm.NewScanner(os.Stdin)
 	for s.Scan() {
-		fmt.Println(s.Entry())
+		if err := tmpl.Execute(out, s.Entry()); err != nil {
+			fatalf("%s\n", err)
+		}
+		if out.NeedNL() {
+			out.WriteNL()
+		}
 	}
+}
+
+// TrackingWriter tracks the last byte written on every write so
+// we can avoid printing a newline if one was already written or
+// if there is no output at all.
+type TrackingWriter struct {
+	w    io.Writer
+	last byte
+}
+
+func newTrackingWriter(w io.Writer) *TrackingWriter {
+	return &TrackingWriter{
+		w:    w,
+		last: '\n',
+	}
+}
+
+func (t *TrackingWriter) Write(p []byte) (n int, err error) {
+	n, err = t.w.Write(p)
+	if n > 0 {
+		t.last = p[n-1]
+	}
+	return
+}
+
+var nl = []byte{'\n'}
+
+func (t *TrackingWriter) WriteNL() (int, error) {
+	return t.w.Write(nl)
+}
+
+func (t *TrackingWriter) NeedNL() bool {
+	return t.last != '\n'
+}
+
+func fatalf(f string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, f, args...)
+	os.Exit(1)
 }
