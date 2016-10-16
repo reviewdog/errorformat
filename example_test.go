1b0ef93824eefca2ad6e0bb322edcd1cc4a3055b
haya14busa <hayabusa1419@gmail.com>
2016-10-16 16:22:55 +0900

    add example test

---
+++ example_test.go
@@ -0,0 +1,25 @@
+package errorformat
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ExampleErrorformat() {
+	in := `
+golint.new.go:3:5: exported var V should have comment or be unexported
+golint.new.go:5:5: exported var NewError1 should have comment or be unexported
+golint.new.go:7:1: comment on exported function F should be of the form "F ..."
+golint.new.go:11:1: comment on exported function F2 should be of the form "F2 ..."
+`
+	efm, _ := NewErrorformat([]string{`%f:%l:%c: %m`, `%-G%.%#`})
+	s := efm.NewScanner(strings.NewReader(in))
+	for s.Scan() {
+		fmt.Println(s.Entry())
+	}
+	// Output:
+	// golint.new.go|3 col 5| exported var V should have comment or be unexported
+	// golint.new.go|5 col 5| exported var NewError1 should have comment or be unexported
+	// golint.new.go|7 col 1| comment on exported function F should be of the form "F ..."
+	// golint.new.go|11 col 1| comment on exported function F2 should be of the form "F2 ..."
+}
