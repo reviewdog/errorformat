6ce4af8d470238070aa110ffb7cd94de16b5284c
haya14busa <hayabusa1419@gmail.com>
2016-10-23 04:41:49 +0900

    move scala test data

--- fmts/testdata/sbt.scala
+++
@@ -1,4 +0,0 @@
-object G {
-  private val unused = 1
-  private def unusedF = {}
-}
--- fmts/testdata/scalac.scala
+++
@@ -1,7 +0,0 @@
-// scalac -Ywarn-unused scalac.scala &> scalac.in
-// sbt -no-colors compile > sbt.in
-object F {
-  private val unused = 1
-  private def unusedF = {}
-  error
-}
