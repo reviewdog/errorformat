111f1a13a0b9c140707410b081b44361116d5c0f
haya14busa <hayabusa1419@gmail.com>
2016-10-23 00:58:11 +0900

    add errorformat for go vet

--- fmts/go.go
+++ fmts/go.go
@@ -9,4 +9,14 @@ func init() {
 		Description: "linter for Go source code",
 		URL:         "https://github.com/golang/lint",
 	})
+
+	register(&Fmt{
+		Name: "govet",
+		Errorformat: []string{
+			`%f:%l: %m`,
+			`%-G%.%#`,
+		},
+		Description: "Vet examines Go source code and reports suspicious problems",
+		URL:         "https://golang.org/cmd/vet/",
+	})
 }
