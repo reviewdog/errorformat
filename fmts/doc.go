8350ae906e5f1cd8d0451712ee924d6955ffa9bc
haya14busa <hayabusa1419@gmail.com>
2016-10-25 08:21:13 +0900

    go generate ./...

--- fmts/doc.go
+++ fmts/doc.go
@@ -5,6 +5,9 @@
 // 	go
 // 		golint	linter for Go source code - https://github.com/golang/lint
 // 		govet	Vet examines Go source code and reports suspicious problems - https://golang.org/cmd/vet/
+// 	javascript
+// 		eslint	(eslint [-f stylish]) A fully pluggable tool for identifying and reporting on patterns in JavaScript - https://github.com/eslint/eslint
+// 		eslint-compact	(eslint -f compact) A fully pluggable tool for identifying and reporting on patterns in JavaScript - https://github.com/eslint/eslint
 // 	scala
 // 		sbt	the interactive build tool - http://www.scala-sbt.org/
 // 		sbt-scalastyle	Scalastyle - SBT plugin - http://www.scalastyle.org/sbt.html
