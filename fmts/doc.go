e26a8f5135f4b767a033c4bcf4b129a68a03f4ed
rhysd <lin90162@yahoo.co.jp>
2016-10-24 23:28:44 +0900

    Revert "Add missing doc for stylelint"
    because it should be generated by go generate.
    
    This reverts commit 43058df55cfe2a19dd7f407dde43ac5859732da7.

--- fmts/doc.go
+++ fmts/doc.go
@@ -1,9 +1,7 @@
 // Package fmts holds defined errorformats.
 //
 // Defined formats:
-//
-// 	css
-// 		stylelint	A mighty modern CSS linter - https://github.com/stylelint/stylelint
+// 
 // 	go
 // 		golint	linter for Go source code - https://github.com/golang/lint
 // 		govet	Vet examines Go source code and reports suspicious problems - https://golang.org/cmd/vet/
