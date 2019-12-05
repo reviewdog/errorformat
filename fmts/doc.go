6951c39af020033f491aa8aba38b9798519b8c0d
Mikhail Grachev <work@mgrachev.com>
2019-12-05 16:28:35 +0300

    Add format for cargo-check
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- fmts/doc.go
+++ fmts/doc.go
@@ -27,6 +27,7 @@
 // 		reek	(reek --single-line) Code smell detector for Ruby - https://github.com/troessner/reek
 // 		rubocop	A Ruby static code analyzer, based on the community Ruby style guide - https://github.com/rubocop-hq/rubocop
 // 	rust
+// 		cargo-check	(cargo check -q --message-format=short) Check a local package and all of its dependencies for errors - https://github.com/rust-lang/cargo
 // 		clippy	(cargo clippy -q --message-format=short) A bunch of lints to catch common mistakes and improve your Rust code - https://github.com/rust-lang/rust-clippy
 // 	scala
 // 		sbt	the interactive build tool - http://www.scala-sbt.org/
