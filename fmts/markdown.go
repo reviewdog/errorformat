b7afc78df3963ef237a0f51a3ca19b5130ff94b6
rickstaa <rick.staa@outlook.com>
2020-11-02 15:18:38 +0100

    :sparkles: Adds markdown language support
    I added markdown language support using the https://github.com/remarkjs/remark-lint
    markdown linter.

---
+++ fmts/markdown.go
@@ -0,0 +1,19 @@
+package fmts
+
+func init() {
+	const lang = "markdown"
+
+	register(&Fmt{
+		Name: "remark-lint",
+		Errorformat: []string{
+			`%P%f`,
+			`%#%l:%c %# %trror  %m`,
+			`%#%l:%c %# %tarning  %m`,
+			`%-Q`,
+			`%-G%.%#`,
+		},
+		Description: "Tool for writing clean and consistent markdown code.",
+		URL:         "https://github.com/remarkjs/remark-lint",
+		Language:    lang,
+	})
+}
