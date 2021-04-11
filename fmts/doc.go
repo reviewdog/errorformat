557667e34dfd98a61e81b954e8426bb19fd0273d
ayatk <git@ayatk.com>
2021-04-11 14:55:12 +0900

    Sort isort docs

--- fmts/doc.go
+++ fmts/doc.go
@@ -41,8 +41,8 @@
 // 	python
 // 		black	A uncompromising Python code formatter - https://github.com/psf/black
 // 		flake8	Tool for python style guide enforcement - https://flake8.pycqa.org/
-// 		pep8	Python style guide checker - https://pypi.python.org/pypi/pep8
 // 		isort	A Python utility / library to sort Python imports - https://github.com/PyCQA/isort
+// 		pep8	Python style guide checker - https://pypi.python.org/pypi/pep8
 // 	ruby
 // 		brakeman	(brakeman --quiet --format tabs) A static analysis security vulnerability scanner for Ruby on Rails applications - https://github.com/presidentbeef/brakeman
 // 		fasterer	Speed improvements suggester - https://github.com/DamirSvrtan/fasterer
